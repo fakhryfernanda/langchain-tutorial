a88bf8be88b03b60feb084d47f7686497ff67c12
Fakhry Fernanda <mff023@gmail.com>
2025-10-18 20:36:31 +0700

    Implement agent with context, tools, structured response, and memory

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
-import json
-import pprint
 from dotenv import load_dotenv
+from dataclasses import dataclass
+
 from langchain.agents import create_agent
+from langchain.tools import tool, ToolRuntime
+from langgraph.checkpoint.memory import InMemorySaver
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
@@ -16,31 +18,67 @@ llm = ChatGoogleGenerativeAI(
     max_retries=2,
 )
 
-messages = [
-    (
-        "system",
-        "You are a helpful assistant that translates English to Indonesian. Translate the user sentence.",
-    ),
-    ("human", "I love you, you love me. We are happy family."),
-]
-ai_msg = llm.invoke(messages)
-print("Translate:", ai_msg)
-print()
 
-def get_weather(city: str) -> str:
+system_prompt = """
+You are an expert weather forecaster, who speaks in puns.
+
+You have access to two tools:
+
+- get_weather_for_location: use this to get the weather for a specific location
+- get_user_location: use this to get the user's location
+
+If a user asks you for the weather, make sure you know the location. If you can tell from the question that they mean wherever they are, use the get_user_location tool to find their location."""
+
+@tool
+def get_weather_for_location(city: str) -> str:
     """Get weather for a given city."""
     return f"It's always sunny in {city}!"
 
+@dataclass
+class Context:
+    """Custom runtime context schema."""
+    user_id: str
+
+@tool
+def get_user_location(runtime: ToolRuntime[Context, str]) -> str:
+    """Retrieve user information based on user ID."""
+    user_id = runtime.context.user_id
+    return "Florida" if user_id == "1" else "SF"
+
+@dataclass
+class ResponseFormat:
+    """Response schema for the agent."""
+    punny_response: str
+    weather_conditions: str | None = None
+
+checkpointer = InMemorySaver()
+
 agent = create_agent(
     model=llm,
-    tools=[get_weather],
-    system_prompt="You are a helpful assistant",
+    system_prompt=system_prompt,
+    tools=[get_user_location, get_weather_for_location],
+    context_schema=Context,
+    response_format=ResponseFormat,
+    checkpointer=checkpointer
 )
 
-# Run the agent
-weather = agent.invoke(
-    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+# `thread_id` is a unique identifier for a given conversation.
+config = {"configurable": {"thread_id": "1"}}
+
+response = agent.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather outside?"}]},
+    config=config,
+    context=Context(user_id="1")
 )
 
-print("Weather report:")
-print(weather)
+print()
+print(response['structured_response'])
+
+# Note that we can continue the conversation using the same `thread_id`.
+response = agent.invoke(
+    {"messages": [{"role": "user", "content": "thank you!"}]},
+    config=config,
+    context=Context(user_id="1")
+)
+
+print(response['structured_response'])
