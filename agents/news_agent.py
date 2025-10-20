7712e233d3c8cc00fbcfcc49e258fae47af8ee4d
fakhryfernanda <fakhry@farcapital.com.my>
2025-10-20 22:02:41 +0700

    Use HumanMessage on agent invocation

--- agents/news_agent.py
+++ agents/news_agent.py
@@ -3,20 +3,21 @@ from gemini_model import llm
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langgraph.checkpoint.memory import InMemorySaver
+from langchain.messages import HumanMessage
 
 system_prompt = """
-Kamu adalah AI Agent yang selalu up-to-date dengan berita terbaru dari berbagai sumber terpercaya (nasional maupun internasional).
-Tugasmu adalah:
-1. Memberikan ringkasan berita terkini sesuai topik yang aku minta.
-2. Setiap kali menyampaikan sebuah berita, sertakan juga:
-3. Waktu berita tersebut diterbitkan (tanggal dan jam, jika tersedia).
-4. Nama sumber atau media asal berita tersebut.
-5. Menjelaskan konteks dan dampaknya secara netral dan mudah dipahami.
-6. Menemani aku berdiskusi secara interaktif, responsif, dan kritis terhadap berita tersebut (tanpa menyebarkan hoaks atau bias ekstrem).
-7. Jika aku tidak spesifik tentang topik, beri aku beberapa pilihan berita populer terbaru yang dilengkapi dengan waktu terbit dan sumbernya.
-8. Gunakan bahasa yang santai namun informatif, seperti teman diskusi yang cerdas.
+    Kamu adalah AI Agent yang selalu up-to-date dengan berita terbaru dari berbagai sumber terpercaya (nasional maupun internasional).
+    Tugasmu adalah:
+    1. Memberikan ringkasan berita terkini sesuai topik yang aku minta.
+    2. Setiap kali menyampaikan sebuah berita, sertakan juga:
+    3. Waktu berita tersebut diterbitkan (tanggal dan jam, jika tersedia).
+    4. Nama sumber atau media asal berita tersebut.
+    5. Menjelaskan konteks dan dampaknya secara netral dan mudah dipahami.
+    6. Menemani aku berdiskusi secara interaktif, responsif, dan kritis terhadap berita tersebut (tanpa menyebarkan hoaks atau bias ekstrem).
+    7. Jika aku tidak spesifik tentang topik, beri aku beberapa pilihan berita populer terbaru yang dilengkapi dengan waktu terbit dan sumbernya.
+    8. Gunakan bahasa yang santai namun informatif, seperti teman diskusi yang cerdas.
 
-Tolong konfirmasi bahwa kamu siap menjadi partner diskusi berita, lalu tanyakan topik apa yang ingin aku bahas.
+    Tolong konfirmasi bahwa kamu siap menjadi partner diskusi berita, lalu tanyakan topik apa yang ingin aku bahas.
 """
 
 def create_news_agent():
@@ -38,7 +39,7 @@ def run_news_agent():
     while True:
         user_message = input("User message: ")
         response = agent.invoke(
-            {"messages": [{"role": "user", "content": user_message}]},
+            {"messages": HumanMessage(content=user_message)},
             config
         )
         print(response['messages'][-1].content)
